724072026011e984a4463cad0ff71734a3297521
euler <euler_l@hotmai.com>
2024-04-19 15:33:41 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApp.UserInputDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿//Declare variables
+using System.Xml;
+
+string name = string.Empty;
+int age = 0;
+int retirementAge = 65;
+
+Console.WriteLine("Please inter your name!");
+name = Console.ReadLine();
+Console.WriteLine("Please inter your age!");
+age = Convert.ToInt32(Console.ReadLine());
+
+// Process the data
+int workingYearsRemaining = retirementAge - age;
+
+// OutputMethod the results to the user
+Console.WriteLine($"Full name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Working years remaining: {workingYearsRemaining}");
---
+++ ConsoleApp.VariableDemo/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+string name = "Yasuo Fushimi";
+int myAge = 54;
+
+Console.WriteLine(name);
+Console.WriteLine("I am " + name);
+Console.WriteLine($"They call me {name}");
+Console.WriteLine("I was given the name {0}", name);
+
+Console.WriteLine("I am " + name + myAge + myAge);
